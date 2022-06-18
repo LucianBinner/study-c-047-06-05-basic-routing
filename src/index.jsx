bde83383ef85f94dd5c284c9531ae38b9e72dfb3
Lucian Binner <lucian.binner@hotmail.com>
2022-06-18 11:41:05 -0300

    feat: index routes

--- src/index.jsx
+++ src/index.jsx
@@ -13,6 +13,15 @@ root.render(
       <Route path="/" element={<App />}>
         <Route path="expenses" element={<Expenses />} />
         <Route path="invoices" element={<Invoices />}>
+          {/* A rota abaixo será reenderiza como paradão no aninhamento invoices */}
+          <Route
+            index
+            element={
+              <main style={{ padding: '1rem' }}>
+                <p>Select an invoice</p>
+              </main>
+            }
+          />
           <Route path=":invoiceId" element={<Invoice />} />
         </Route>
         <Route
