490ed636f0cd36dc3e57e999244412d27f0d7d31
Lucian Binner <lucian.binner@hotmail.com>
2022-06-18 11:52:52 -0300

    feat: search params

--- src/routes/invoices.jsx
+++ src/routes/invoices.jsx
@@ -1,8 +1,12 @@
-import { NavLink, Outlet } from 'react-router-dom';
+import { NavLink, Outlet, useSearchParams } from 'react-router-dom';
 import { getInvoices } from '../data';
 
 export default function Invoices() {
   let invoices = getInvoices();
+  let [searchParams, setSearchParams] = useSearchParams();
+  {
+    /* useSearchParams - Pesquisa parametros passados na url e também seta parametros na url  */
+  }
   return (
     <div style={{ display: 'flex' }}>
       <nav
@@ -11,24 +15,42 @@ export default function Invoices() {
           padding: '1rem',
         }}
       >
+        <input
+          value={searchParams.get('filter') || ''}
+          onChange={(event) => {
+            let filter = event.target.value;
+            if (filter) {
+              setSearchParams({ filter });
+            } else {
+              setSearchParams({});
+            }
+          }}
+        />
         {/* A vantagem do NavLink para o Link é que o NavLink falar quando um link esta ativo ou não, assim abrindo várias possibilidades */}
         {/* A flag de verificação se está ativo ou não também funciona com o className, conforme abaixo */}
         {/* <NavLink className={({ isActive }) => isActive ? "red" : "blue"} /> */}
-        {invoices.map((invoice) => (
-          <NavLink
-            style={({ isActive }) => {
-              return {
-                display: 'block',
-                margin: '1rem 0',
-                color: isActive ? 'red' : '',
-              };
-            }}
-            to={`/invoices/${invoice.number}`}
-            key={invoice.number}
-          >
-            {invoice.name}
-          </NavLink>
-        ))}
+        {invoices
+          .filter((invoice) => {
+            let filter = searchParams.get('filter');
+            if (!filter) return true;
+            let name = invoice.name.toLowerCase();
+            return name.startsWith(filter.toLowerCase());
+          })
+          .map((invoice) => (
+            <NavLink
+              style={({ isActive }) => {
+                return {
+                  display: 'block',
+                  margin: '1rem 0',
+                  color: isActive ? 'red' : '',
+                };
+              }}
+              to={`/invoices/${invoice.number}`}
+              key={invoice.number}
+            >
+              {invoice.name}
+            </NavLink>
+          ))}
       </nav>
       <Outlet /> {/* Renderiza as rotas filhas */}
     </div>
