a95da8bd8fd1b05e2b0e6a2b0074e59d10841b86
Lucian Binner <lucian.binner@hotmail.com>
2022-06-18 11:12:13 -0300

    feat: basic routing

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Home from './templates/Home';
+import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Expenses from './routes/expenses';
+import Invoices from './routes/invoices';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
-    <Home />
-  </>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="expenses" element={<Expenses />} />
+      <Route path="invoices" element={<Invoices />} />
+    </Routes>
+  </BrowserRouter>,
 );
